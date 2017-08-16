01b82712e32acdf3dfcddaefbf26193540f70ecb
jobshihainan <jobshihainan@gmail.com>
2017-08-16 14:08:18 +0800

    initial files conf/ www/

---
+++ www/config_override.py
@@ -0,0 +1,12 @@
+"""
+Override configurations.
+"""
+
+configs = {
+    'db': {
+        'host': "192.168.56.101",
+        "user": "root",
+        "password": "123456",
+        "db": 'db_localhost'
+    }
+}
